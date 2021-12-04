7819ac0fea8cf78f416cccef63c16d76dc78cbef
Nico Vibert <nico.vibert@hashicorp.com>
2021-12-04 17:37:23 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ variable "availability_zone" {
   
  
 output "splunk_public_ip" {
-  value = module.splunk.splunk.public_ip
+  value = module.splunk.splunk_public_ip
 }
 output "splunk_default_username" {
   value = module.splunk.splunk_default_username
